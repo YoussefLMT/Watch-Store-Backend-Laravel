9c23a63057f49a63c980977bba8687ae78afccde
YoussefLMT <mcyoussef324@gmail.com>
2022-11-30 18:40:20 +0100

    add routes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
