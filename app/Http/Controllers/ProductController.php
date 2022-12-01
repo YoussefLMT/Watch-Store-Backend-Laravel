085bdb19cf739deb07841cba47637d58620d06f3
YoussefLMT <mcyoussef324@gmail.com>
2022-12-01 19:28:16 +0100

    add index function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
@@ -13,7 +14,12 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = Product::all();
+
+        return response()->json([
+            'status' => 200,
+            'products' => $products,
+        ]);
     }
 
     /**
